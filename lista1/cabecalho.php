a2d379bccf5f6faa7e1df2c5a87be0bea03e0737
lunwes <lunwees@gmail.com>
2025-09-18 21:10:07 -0300

    implementação dos exercícios 1, 2, 3, 4 e 5.

---
+++ lista1/cabecalho.php
@@ -0,0 +1,53 @@
+
+<body>
+<nav class="navbar navbar-expand-lg navbar-dark bg-primary">
+  <div class="container">
+    <a class="navbar-brand" href="#">Lista 1</a>
+    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Alternar navegação">
+      <span class="navbar-toggler-icon"></span>
+    </button>
+    <div class="collapse navbar-collapse" id="navbarSupportedContent">
+      <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+        
+<li class="nav-item dropdown">
+  <a class="nav-link dropdown-toggle" href="#" id="dropdown1" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+    Exercícios de 1 a 10
+  </a>
+  <ul class="dropdown-menu" aria-labelledby="dropdown1">
+    <li><a class="dropdown-item" href="exercicio1.php">Exercício 1</a></li>
+    <li><a class="dropdown-item" href="exercicio2.php">Exercício 2</a></li>
+    <li><a class="dropdown-item" href="exercicio3.php">Exercício 3</a></li>
+    <li><a class="dropdown-item" href="exercicio4.php">Exercício 4</a></li>
+    <li><a class="dropdown-item" href="exercicio5.php">Exercício 5</a></li>
+    <li><a class="dropdown-item" href="exercicio6.php">Exercício 6</a></li>
+    <li><a class="dropdown-item" href="exercicio7.php">Exercício 7</a></li>
+    <li><a class="dropdown-item" href="exercicio8.php">Exercício 8</a></li>
+    <li><a class="dropdown-item" href="exercicio9.php">Exercício 9</a></li>
+    <li><a class="dropdown-item" href="exercicio10.php">Exercício 10</a></li>
+  </ul>
+</li>
+
+<li class="nav-item dropdown">
+  <a class="nav-link dropdown-toggle" href="#" id="dropdown2" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+    Exercícios de 11 a 20
+  </a>
+  <ul class="dropdown-menu" aria-labelledby="dropdown2">
+  <li><a class="dropdown-item" href="exercicio11.php">Exercício 11</a></li>
+    <li><a class="dropdown-item" href="exercicio12.php">Exercício 12</a></li>
+    <li><a class="dropdown-item" href="exercicio13.php">Exercício 13</a></li>
+    <li><a class="dropdown-item" href="exercicio14.php">Exercício 14</a></li>
+    <li><a class="dropdown-item" href="exercicio15.php">Exercício 15</a></li>
+    <li><a class="dropdown-item" href="exercicio16.php">Exercício 16</a></li>
+    <li><a class="dropdown-item" href="exercicio17.php">Exercício 17</a></li>
+    <li><a class="dropdown-item" href="exercicio18.php">Exercício 18</a></li>
+    <li><a class="dropdown-item" href="exercicio19.php">Exercício 19</a></li>
+    <li><a class="dropdown-item" href="exercicio20.php">Exercício 20</a></li>
+  </ul>
+</li>
+      </ul>
+    </div>
+  </div>
+</nav>
+<div class="container py-3"> <!-- Comentário em HTML -->
+
+    
---
+++ lista1/exercicio2.php
@@ -0,0 +1,41 @@
+<!doctype html>
+<html lang="en">
+
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Subtração de Números</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet">
+</head>
+
+<?php
+
+include("cabecalho.php");
+?>
+
+<div class="container">
+    <h1>Exercício 2</h1>
+    <form method="post">
+        <div class="mb-3">
+            <label for="valor1" class="form-label">Digite o primeiro valor:</label>
+            <input type="number" id="valor1" name="valor1" class="form-control" required="">
+        </div>
+        <div class="mb-3">
+            <label for="valor2" class="form-label">Digite um número a ser subtraído do valor acima:</label>
+            <input type="number" id="valor2" name="valor2" class="form-control" required="">
+        </div>
+        <button type="submit" class="btn btn-primary mb-3">Enviar</button>
+    </form>
+
+
+    <?php   
+        if ($_SERVER['REQUEST_METHOD'] == 'POST') {
+        $n1 = $_POST['valor1'];
+        $n2 = $_POST['valor2'];
+        $resultado = $n1 - $n2;
+        echo "O resultado é $resultado";
+        // - * ** / // %
+        }
+
+        include("rodape.php");
+    ?>
